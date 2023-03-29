bb310204b81d5efbbce620fee1d029a6adfe8a4d
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-03-29 16:19:37 +0400

    sd
    asda

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['life-cdn.lifepharmacy.com', 'lifeadmin-app.s3.me-south-1.amazonaws.com', 'www.lifepharmacy.com'],
-  },
   experimental: {
     appDir: true,
-  }
-}
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/about',
-        permanent: true,
-      },
-    ]
   },
 }
+
 module.exports = nextConfig
